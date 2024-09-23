2f824b2b97701dc7b8e931cf3412d56bcfe37ea0
Pau2Kol <flibuster.pau@outlook.fr>
2024-09-23 11:43:24 +0200

    qsfdgh

--- Hangman/main.go
+++ Hangman/main.go
@@ -1,16 +1,19 @@
 package main
 
 func main() {
-
+	mot := mot()
+	pendu(mot)
 }
 
 func mot() []string {
 	//choisir un mot dans le txt
 	//utiliser la function rdm de math pour avoir un mort aléatoire
 	//mettre le mot en full maj + en Array
-	//Donner le mot e nfull maj a la function main
+	//Donner le mot en full maj a la function main
 
 }
-func printlependuetletextcettefonctionestbeaucouptroplonguejesais() {
-
+func pendu(mot []string) {
+	//vérifier si le mot contient la lettre
+	//Print le pendu si oui
+	//si non bah juste retourne le mot avec le nombre d'essai et la lettre en plus
 }
