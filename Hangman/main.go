d8fe82234825b61dfd2b95f2f595d448959d322d
Pau2Kol <flibuster.pau@outlook.fr>
2024-09-23 10:28:05 +0200

    Je test des trucs la

--- Hangman/main.go
+++ Hangman/main.go
@@ -1,1 +1,16 @@
 package main
+
+func main() {
+
+}
+
+func mot() []string {
+	//choisir un mot dans le txt
+	//utiliser la function rdm de math pour avoir un mort aléatoire
+	//mettre le mot en full maj + en Array
+	//Donner le mot e nfull maj a la function main
+
+}
+func printlependuetletextcettefonctionestbeaucouptroplonguejesais() {
+
+}
